162012393a658cec38d77c0d765d9f1481e7274f
Abhishek_ <vsukumar0991@gmail.com>
2023-12-17 14:07:03 +0530

    Encapsulation code changes has been added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,10 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String args[]){
+        RWOnly r  = new RWOnly("Abhishek");
+
+        r.setName("Aditi");
+        System.out.println(r.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private  String Name;
+
+    public RWOnly(String name) {
+        this.Name = name;
+        System.out.println(this.Name);
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
 }
